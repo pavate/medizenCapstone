eded6ac86bd87e1fd8e342fd9dfc2afe27cad632
pavate <50588499+pavate@users.noreply.github.com>
2021-04-01 09:27:53 -0400

    Update EditProfile.kt

--- app/src/main/java/org/sheridan/capstoneprototype/EditProfile.kt
+++ app/src/main/java/org/sheridan/capstoneprototype/EditProfile.kt
@@ -2,6 +2,7 @@ package org.sheridan.capstoneprototype
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
 import org.jetbrains.anko.doAsync
@@ -9,43 +10,49 @@ import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import org.sheridan.capstoneprototype.database.User.UserDatabase
 import org.sheridan.capstoneprototype.database.User.UserEntity
+import org.sheridan.capstoneprototype.databinding.ActivityEditProfileBinding
 
 class EditProfile : AppCompatActivity() {
     private lateinit var mDb: UserDatabase
+    private lateinit var binding: ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         mDb = UserDatabase.getInstance(applicationContext)
+        binding = ActivityEditProfileBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
 
-        val etName: TextView = findViewById(R.id.etName)
-        val etEmail: TextView = findViewById(R.id.etEmail)
-        val etAge: TextView = findViewById(R.id.etAge)
-        val etPassword: TextView = findViewById(R.id.etPassword)
-        val btnSave: Button = findViewById(R.id.btnSave)
+        binding.btnSave.setOnClickListener {
 
-//        val name = etName.getText().toString()
-//        val uEmail = etEmail.getText().toString()
-//        val age = etAge.getText().toString().toInt()
-//        val pasword = etPassword.getText().toString()
+            var fullName = binding.etName.text.toString()
+            var userEmail = binding.etEmail.text.toString()
+            var age =  binding.etAge.text.toString()
+            var userPassword = binding.etPassword.text.toString()
+            var userPassword2 = binding.etPassword2.text.toString()
 
-
-        btnSave.setOnClickListener {
-            val user = UserEntity(
-                0,
-                fullName = "testuser",
-                userEmail = "testuser@gmail.com",
-                age =  35,
-                userPassword = "password"
-            )
-            doAsync {
-                // do things in the background  // (1)
-                mDb.userDao().insert(user)     // Put the student in database
-                uiThread {
-                    // make changes to the UI   // (2)
-                    toast("Record : " + user.fullName + " inserted.")
+            if (fullName.isNotEmpty() && userEmail.isNotEmpty() && age.isNotEmpty()) {
+                if (userPassword.equals(userPassword2)) {
+                    val user = UserEntity(
+                            0,
+                            fullName,
+                            userEmail,
+                            age.toInt(),
+                            userPassword
+                    )
+                    doAsync {
+                        // do things in the background  // (1)
+                        mDb.userDao().insert(user)     // Put the student in database
+                        uiThread {
+                            // make changes to the UI   // (2)
+                            toast("Record : " + user.fullName + " inserted.").show()
+                        }
+                    }
+                } else {
+                    toast("Password doesn't match").show()
                 }
+            } else {
+                toast("Missing user input").show()
             }
         }
-
     }
 }
